1d0ff5570c4475bf2901071930ff0c514459cfdf
tahmed4 <55896104+tahmed4@users.noreply.github.com>
2020-12-29 17:58:10 +0000

    Jsh working with threads

---
+++ src/main/java/uk/ac/ucl/jsh/RunCommand.java
@@ -0,0 +1,35 @@
+package uk.ac.ucl.jsh;
+
+import java.io.PipedInputStream;
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import uk.ac.ucl.jsh.app.Application;
+import uk.ac.ucl.jsh.app.ApplicationFactory;
+
+public class RunCommand implements Runnable{
+    ArrayList<String> tokens;
+    OutputStream out;
+    InputStream in;
+
+    public RunCommand(ArrayList<String> tokens, OutputStream out, InputStream in){
+        this.tokens = tokens;
+        this.out = out;
+        this.in = in;
+    }
+
+    public void run(){
+        try{
+            String appName = tokens.get(0);
+            ArrayList<String> appArgs = new ArrayList<String>(tokens.subList(1, tokens.size()));
+            Application app = ApplicationFactory.make(appName);
+            app.exec(appArgs, out);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
