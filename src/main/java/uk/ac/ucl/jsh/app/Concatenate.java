55d29827ebab3a5bb7b703f8afac6923d1c17c15
tahmed4 <55896104+tahmed4@users.noreply.github.com>
2021-01-03 19:28:00 +0000

    updated to work with inputstream

--- src/main/java/uk/ac/ucl/jsh/app/Concatenate.java
+++ src/main/java/uk/ac/ucl/jsh/app/Concatenate.java
@@ -14,6 +14,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import uk.ac.ucl.jsh.Jsh;
 import uk.ac.ucl.jsh.app.HelperMethods;
@@ -24,37 +25,31 @@ public class Concatenate implements Application {
         OutputStreamWriter writer = new OutputStreamWriter(out);
 
         if (appArgs.isEmpty()) { // Take InputStream
-            
             String[] pipeInput = HelperMethods.readInputStream(in);
-            for (String line : pipeInput) {
-                writer.write(line);
-                writer.write(System.getProperty("line.separator"));
-                writer.flush();
-            }
+            appArgs = new ArrayList<String>(Arrays.asList(pipeInput));
+            System.out.println(appArgs);
+        }
 
-        } else { // Use file path(s)
-
-            for (String arg : appArgs) {
-                Charset encoding = StandardCharsets.UTF_8;
-                File currFile = new File(Jsh.currentDirectory + File.separator + arg);
-                if (currFile.exists()) {
-                    Path filePath = Paths.get(Jsh.currentDirectory + File.separator + arg);
-                    try (BufferedReader reader = Files.newBufferedReader(filePath, encoding)) {
-                        String line = null;
-                        while ((line = reader.readLine()) != null) {
-                            writer.write(String.valueOf(line));
-                            writer.write(System.getProperty("line.separator"));
-                            writer.flush();
-                        }
-                    } catch (IOException e) {
-                        HelperMethods.outputError(unsafe, out, "cat: cannot open " + arg); return;
+        for (String arg : appArgs) {
+            Charset encoding = StandardCharsets.UTF_8;
+            File currFile = new File(Jsh.currentDirectory + File.separator + arg);
+            if (currFile.exists()) {
+                Path filePath = Paths.get(Jsh.currentDirectory + File.separator + arg);
+                try (BufferedReader reader = Files.newBufferedReader(filePath, encoding)) {
+                    String line = null;
+                    while ((line = reader.readLine()) != null) {
+                        writer.write(String.valueOf(line));
+                        writer.write(System.getProperty("line.separator"));
+                        writer.flush();
                     }
-                } else {
-                    HelperMethods.outputError(unsafe, out, "cat: " + arg + " does not exist"); return;
+                } catch (IOException e) {
+                    HelperMethods.outputError(unsafe, out, "cat: cannot open " + arg); return;
                 }
+            } else {
+                HelperMethods.outputError(unsafe, out, "cat: " + arg + " does not exist"); return;
             }
+        }
 
-        }   
     }
 
 }
