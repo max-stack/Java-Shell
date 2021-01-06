2c00224568a2546cbe350b3f09750c9148d2223f
max-stack <55951090+max-stack@users.noreply.github.com>
2021-01-06 23:25:53 +0000

    Adding to increase branch coverag

--- src/test/java/uk/ac/ucl/jsh/ChangeDirectoryTest.java
+++ src/test/java/uk/ac/ucl/jsh/ChangeDirectoryTest.java
@@ -1,6 +1,7 @@
 package uk.ac.ucl.jsh;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -28,6 +29,7 @@ public class ChangeDirectoryTest {
 
     static File dir1;
     static File dir2;
+    static File file1;
     String temp;
     private final PrintStream standardOut = System.out;
     private final PrintStream standardErr = System.err;
@@ -38,12 +40,23 @@ public class ChangeDirectoryTest {
         dir1 = new File("./dir1");
         dir2 = new File("./dir2");
 
+        file1 = new File("test1.txt");
+
+        if(!file1.exists()){
+            file1.createNewFile();
+        }
+
         if(!dir1.exists()){
             dir1.mkdirs();
         }
         if(!dir2.exists()){
             dir2.mkdirs();
         }
+    }
+
+    @AfterAll
+    static void end() throws Exception{
+        file1.delete();
     }
 
     @BeforeEach
@@ -86,6 +99,16 @@ public class ChangeDirectoryTest {
         ArrayList<String> args = new  ArrayList<String>();
         String wrongDirectory = "wrongdirectory";
         args.add(wrongDirectory);
+
+        new ChangeDirectory().exec(args, null, System.out, false);
+        assertEquals("cd: " + args.get(0) + " is not an existing directory", outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    public void testSafeThrowsExceptionNotDirectory() throws Exception {
+        //Throw exception - unable to open the file.
+        ArrayList<String> args = new  ArrayList<String>();
+        args.add(file1.getPath());
 
         new ChangeDirectory().exec(args, null, System.out, false);
         assertEquals("cd: " + args.get(0) + " is not an existing directory", outputStreamCaptor.toString().trim());
